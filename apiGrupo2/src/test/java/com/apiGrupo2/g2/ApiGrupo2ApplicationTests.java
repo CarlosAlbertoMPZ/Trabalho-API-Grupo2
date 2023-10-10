7370652604b58bada28c30fd11c5227595bd9f64
rayane <rayanebarbosapet@gmail.com>
2023-10-09 21:58:23 -0300

    first commit

---
+++ apiGrupo2/src/test/java/com/apiGrupo2/g2/ApiGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiGrupo2.g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
