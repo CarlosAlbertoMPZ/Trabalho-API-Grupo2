2fc3b9b2142afb23f5e7dabe4d0a7ff573e7a46e
CarlosAlbertoMPZ <carlosalbertompz@gmail.com>
2023-10-23 09:59:28 -0300

    feat: criando cadastro pedido1.

---
+++ apiGrupo2/src/main/java/com/apiGrupo2/g2/dto/PedidoRequestCadastroDTO.java
@@ -0,0 +1,38 @@
+package com.apiGrupo2.g2.dto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PedidoRequestCadastroDTO {
+
+	private String cpfUsuario;
+	List<ProdutoPedidoDTO> produtosPedidos;
+
+	public PedidoRequestCadastroDTO() {
+		super();
+	}
+
+	public PedidoRequestCadastroDTO(String cpfUsuario, List<ProdutoPedidoDTO> produtosPedidos) {
+		super();
+		this.cpfUsuario = cpfUsuario;
+		this.produtosPedidos = produtosPedidos;
+	}
+
+	public String getCpfUsuario() {
+		return cpfUsuario;
+	}
+
+	public void setCpfUsuario(String cpfUsuario) {
+		this.cpfUsuario = cpfUsuario;
+	}
+
+	public List<ProdutoPedidoDTO> getProdutosPedidos() {
+		return produtosPedidos;
+	}
+
+	public void setProdutosPedidos(List<ProdutoPedidoDTO> produtosPedidos) {
+		this.produtosPedidos = produtosPedidos;
+	}
+
+}
---
+++ apiGrupo2/src/main/java/com/apiGrupo2/g2/dto/PedidoResponseCadastroDTO.java
@@ -0,0 +1,83 @@
+package com.apiGrupo2.g2.dto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.apiGrupo2.g2.entities.Pedido;
+import com.apiGrupo2.g2.entities.Produto;
+
+public class PedidoResponseCadastroDTO {
+
+	private Integer idPedido;
+	private LocalDateTime dataPedido;
+	private Boolean ativo;
+	private String cpfUsuario;
+	private Double valorTotalPedido;
+	List<ProdutoPedidoResponse> produtosPedidosResponse = new ArrayList<>();
+
+	public PedidoResponseCadastroDTO() {
+		super();
+	}
+
+	public PedidoResponseCadastroDTO(Pedido pedido) {
+		super();
+		this.idPedido = pedido.getId();
+		this.dataPedido = pedido.getDataPedido();
+		this.ativo = pedido.getAtivo();
+		this.cpfUsuario = pedido.getUsuario().getCpf();
+		this.valorTotalPedido = pedido.getValorPedido();
+		this.produtosPedidosResponse = produtosPedidosResponse;
+		
+		
+	}
+
+	public Double getValorTotalPedido() {
+		return valorTotalPedido;
+	}
+
+	public void setValorTotalPedido(Double valorTotalPedido) {
+		this.valorTotalPedido = valorTotalPedido;
+	}
+
+	public Integer getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Integer idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public LocalDateTime getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(LocalDateTime dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getCpfUsuario() {
+		return cpfUsuario;
+	}
+
+	public void setCpfUsuario(String cpfUsuario) {
+		this.cpfUsuario = cpfUsuario;
+	}
+
+	public List<ProdutoPedidoResponse> getProdutosPedidosResponse() {
+		return produtosPedidosResponse;
+	}
+
+	public void setProdutosPedidosResponse(List<ProdutoPedidoResponse> produtosPedidosResponse) {
+		this.produtosPedidosResponse = produtosPedidosResponse;
+	}
+
+}
