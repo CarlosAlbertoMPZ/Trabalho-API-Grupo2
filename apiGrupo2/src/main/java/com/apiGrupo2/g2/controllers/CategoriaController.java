815b9876b43a0d2bceceb9a188e3390c0ecb3066
CarlosAlbertoMPZ <carlosalbertompz@gmail.com>
2023-10-24 13:13:59 -0300

    feat: Alterando tipo de retorno de categoria.

--- apiGrupo2/src/main/java/com/apiGrupo2/g2/controllers/CategoriaController.java
+++ apiGrupo2/src/main/java/com/apiGrupo2/g2/controllers/CategoriaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.mail.MessagingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,54 +21,72 @@ import com.apiGrupo2.g2.entities.Categoria;
 import com.apiGrupo2.g2.services.CategoriaService;
 import com.apiGrupo2.g2.services.EmailService;
 
+import exceptions.MyEntityNotFoundException;
+
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
 
 	private EmailService emailService;
-    @Autowired
-    public void setEmailService(EmailService emailService) {
-        this.emailService = emailService;
-    }
-	
+
+	@Autowired
+	public void setEmailService(EmailService emailService) {
+		this.emailService = emailService;
+	}
+
 	@Autowired
 	CategoriaService categoriaService;
-	
+
 	@GetMapping("/contar")
-	public Integer getContar() throws MessagingException{
-		//emailService.envioEmailCadastro();
+	public Integer getContar() throws MessagingException {
+		// emailService.envioEmailCadastro();
 		emailService.envioEmailTeste();
 		return categoriaService.getContar();
 	}
-	
+
 	@PostMapping("/salvar")
-	public CategoriaResponseDTO salvar (@RequestBody CategoriaRequestCadastroDTO catReqDTO) {
+	public CategoriaResponseDTO salvar(@RequestBody CategoriaRequestCadastroDTO catReqDTO) {
 		return categoriaService.salvar(catReqDTO);
 	}
-	
+
 	@GetMapping("/listar")
-	public List <Categoria> listar(){
-		return categoriaService.listar();
+	public ResponseEntity<List<CategoriaResponseDTO>> listar() {
+		return ResponseEntity.ok(categoriaService.listar());
 	}
-	
+
 	@GetMapping("/{id}")
-	public Categoria acharId(@PathVariable Integer id) {
-		return categoriaService.acharId(id);
+	public ResponseEntity<CategoriaResponseDTO> acharId(@PathVariable Integer id) {
+		CategoriaResponseDTO categoriaDTO = categoriaService.acharId(id);
+		if (categoriaDTO == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(categoriaDTO);
+
 	}
-	
+
 //	@DeleteMapping("/delete/{id}")
 //	public void apagar(@PathVariable Integer id) {
 //		categoriaService.apagar(id);
 //	}
-	
+
 	@DeleteMapping("/deletarLogico/{id}")
-	public void deletarLogico(@PathVariable Integer id) {
-		categoriaService.deletarLogico(id);
+	public ResponseEntity<Void> deletarLogico(@PathVariable Integer id) throws MyEntityNotFoundException {
+		boolean deletarLogico = categoriaService.deletarLogico(id);
+		if (deletarLogico) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
-	
+
 	@PutMapping("/atualizar/{id}")
-	public Categoria atualizar(@PathVariable Integer id, @RequestBody Categoria objetoCategoria) { 
-		 return categoriaService.atualizar(id, objetoCategoria);
-	
+	public ResponseEntity<CategoriaResponseDTO> atualizar(@PathVariable Integer id,
+			@RequestBody CategoriaRequestCadastroDTO objetoCategoria) {
+		CategoriaResponseDTO categoriaDTO = categoriaService.atualizar(id, objetoCategoria);
+		if (categoriaDTO == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(categoriaDTO);
 	}
+
 }
